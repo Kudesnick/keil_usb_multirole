7fe657164c8f3df44be86ebf2c62fc9f8528fe37
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2019-08-24 22:57:01 +0300

    modify target chip and output path

---
+++ keil_example/RTE/_STM32F103_Flash/RTE_Components.h
@@ -0,0 +1,35 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HID' 
+ * Target:  'STM32F103 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/*  Keil.MDK-Pro::USB:CORE:Release:6.13.6 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+/*  Keil.MDK-Pro::USB:Device:6.13.6 */
+#define RTE_USB_Device_0                /* USB Device 0 */
+
+/*  Keil.MDK-Pro::USB:Device:HID:6.13.6 */
+#define RTE_USB_Device_HID_0            /* USB Device HID instance 0 */
+
+/*  Keil::CMSIS Driver:USB Device:2.1 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+
+
+#endif /* RTE_COMPONENTS_H */
