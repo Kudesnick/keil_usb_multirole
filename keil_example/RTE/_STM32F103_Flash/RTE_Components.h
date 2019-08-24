35c23820dd435fd4af043ae6847f314349e2acb7
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2019-08-24 23:21:58 +0300

    [mod] add event recorder but not configured this func
    device connected and detected as USB Input device

--- keil_example/RTE/_STM32F103_Flash/RTE_Components.h
+++ keil_example/RTE/_STM32F103_Flash/RTE_Components.h
@@ -19,9 +19,24 @@
 /*  ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
-/*  Keil.MDK-Pro::USB:CORE:Release:6.13.6 */
+/*  Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/*  Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:STDIN:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_ITM       /* Compiler I/O: STDIN ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:TTY:ITM:1.2.0 */
+#define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
+          #define RTE_Compiler_IO_TTY_ITM         /* Compiler I/O: TTY ITM */
+/*  Keil.MDK-Pro::USB:CORE:Debug:6.13.6 */
 #define RTE_USB_Core                    /* USB Core */
-          #define RTE_USB_Core_Release            /* USB Core Release Version */
+          #define RTE_USB_Core_Debug              /* USB Core Debug Version */
 /*  Keil.MDK-Pro::USB:Device:6.13.6 */
 #define RTE_USB_Device_0                /* USB Device 0 */
 
