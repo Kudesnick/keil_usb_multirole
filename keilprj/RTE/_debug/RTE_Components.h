d0970d4b198075bdb3eb43bd8530ad1c654c354d
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2019-08-24 22:41:46 +0300

    base project setting to USB HID debug

--- keilprj/RTE/_debug/RTE_Components.h
+++ keilprj/RTE/_debug/RTE_Components.h
@@ -16,6 +16,13 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.1 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/*  Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
 /*  Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
           #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
@@ -28,6 +35,19 @@
 /*  Keil.ARM Compiler::Compiler:I/O:TTY:ITM:1.2.0 */
 #define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
           #define RTE_Compiler_IO_TTY_ITM         /* Compiler I/O: TTY ITM */
+/*  Keil.MDK-Plus::USB:CORE:Debug:6.13.6 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Debug              /* USB Core Debug Version */
+/*  Keil.MDK-Plus::USB:Device:6.13.6 */
+#define RTE_USB_Device_0                /* USB Device 0 */
+#define RTE_USB_Device_1                /* USB Device 1 */
+#define RTE_USB_Device_2                /* USB Device 2 */
+
+/*  Keil.MDK-Plus::USB:Device:HID:6.13.6 */
+#define RTE_USB_Device_HID_0            /* USB Device HID instance 0 */
+
+/*  Keil::CMSIS Driver:USB Device:2.1 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
 
 
 #endif /* RTE_COMPONENTS_H */
