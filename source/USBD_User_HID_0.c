5896238931ba204d3ff3a5dfebe365176450c56a
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2019-08-26 10:40:31 +0300

    replase hex values to predefined constants

--- source/USBD_User_HID_0.c
+++ source/USBD_User_HID_0.c
@@ -20,104 +20,72 @@ const uint8_t usbd_hid0_report_descriptor[] = {
 
     // Keyboard Top-Level Collection (TLC)
 
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x06,                    // USAGE (Keyboard)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x01,                    //   REPORT_ID (1)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
-    0x95, 0x05,                    //   REPORT_COUNT (5)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x05, 0x08,                    //   USAGE_PAGE (LEDs)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Num Lock)
-    0x29, 0x05,                    //   USAGE_MAXIMUM (Kana)
-    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x03,                    //   REPORT_SIZE (3)
-    0x91, 0x03,                    //   OUTPUT (Cnst,Var,Abs)
-    0x95, 0x06,                    //   REPORT_COUNT (6)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    0xc0,                          // END_COLLECTION
+    HID_UsagePage(HID_USAGE_PAGE_GENERIC),
+    HID_Usage(HID_USAGE_GENERIC_KEYBOARD),
+    HID_Collection(HID_Application),
+        HID_ReportID(0x01),
+        HID_UsagePage(HID_USAGE_PAGE_KEYBOARD),
+        HID_UsageMin(HID_USAGE_KEYBOARD_LCTRL),
+        HID_UsageMax(HID_USAGE_KEYBOARD_RGUI),
+        HID_LogicalMin(0),
+        HID_LogicalMax(1),
+        HID_ReportSize(1),
+        HID_ReportCount(8),
+        HID_Input(HID_Data | HID_Variable | HID_Absolute),
+        HID_ReportCount(1),
+        HID_ReportSize(8),
+        HID_Input(HID_Constant | HID_Variable | HID_Absolute),
+        HID_ReportCount(5),
+        HID_ReportSize(1),
+        HID_UsagePage(HID_USAGE_PAGE_LED),
+        HID_UsageMin(HID_USAGE_LED_NUM_LOCK),
+        HID_UsageMax(HID_USAGE_LED_KANA),
+        HID_Output(HID_Data | HID_Variable | HID_Absolute),
+        HID_ReportCount(1),
+        HID_ReportSize(3),
+        HID_Output(HID_Constant | HID_Variable | HID_Absolute),
+        HID_ReportCount(6),
+        HID_ReportSize(8),
+        HID_LogicalMin(0),
+        HID_LogicalMax(101),
+        HID_UsagePage(HID_USAGE_PAGE_KEYBOARD),
+        HID_UsageMin(0),
+        HID_UsageMax(101),
+        HID_Input(HID_Data | HID_Variable | HID_Absolute),
+    HID_EndCollection,
 
     // Mouse TLC
 
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,                    // USAGE (Mouse)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x02,                    //   REPORT_ID (2)
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x05, 0x09,                    //     USAGE_PAGE (Button)
-    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x75, 0x01,                    //     REPORT_SIZE (1)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x05,                    //     REPORT_SIZE (5)
-    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x02,                    //     REPORT_COUNT (2)
-    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-    0xc0,                          //   END_COLLECTION
-    0xc0                           // END_COLLECTION
+    HID_UsagePage(HID_USAGE_PAGE_GENERIC),
+    HID_Usage(HID_USAGE_GENERIC_MOUSE),
+    HID_Collection(HID_Application),
+        HID_ReportID(0x02),
+        HID_Usage(HID_USAGE_GENERIC_POINTER),
+        HID_Collection(HID_Physical),
+            HID_UsagePage(HID_USAGE_PAGE_BUTTON),
+            HID_UsageMin(1),
+            HID_UsageMax(3),
+            HID_LogicalMin(0),
+            HID_LogicalMax(1),
+            HID_ReportCount(3),
+            HID_ReportSize(1),
+            HID_Input(HID_Data | HID_Variable | HID_Absolute),
+            HID_ReportCount(1),
+            HID_ReportSize(5),
+            HID_Input(HID_Constant | HID_Variable | HID_Absolute),
+            HID_UsagePage(HID_USAGE_PAGE_GENERIC),
+            HID_Usage(HID_USAGE_GENERIC_X),
+            HID_Usage(HID_USAGE_GENERIC_Y),
+            HID_Usage(HID_USAGE_GENERIC_WHEEL),
+            HID_LogicalMin((uint8_t)(-127)),
+            HID_LogicalMax(127),
+            HID_ReportSize(8),
+            HID_ReportCount(3),
+            HID_Input(HID_Data | HID_Variable | HID_Relative),
+        HID_EndCollection,
+    HID_EndCollection,
 };
-/*    HID_UsagePage(HID_USAGE_PAGE_GENERIC),
-  HID_Usage(HID_USAGE_GENERIC_KEYBOARD),
-  HID_Collection(HID_Application),
-    HID_UsagePage(HID_USAGE_PAGE_KEYBOARD),
-    HID_UsageMin(HID_USAGE_KEYBOARD_LCTRL),
-    HID_UsageMax(HID_USAGE_KEYBOARD_RGUI),
-    HID_LogicalMin(0),
-    HID_LogicalMax(1),
-    HID_ReportSize(1),
-    HID_ReportCount(8),
-    HID_Input(HID_Data | HID_Variable | HID_Absolute),
-    HID_ReportCount(1),
-    HID_ReportSize(8),
-    HID_Input(HID_Data | HID_Variable | HID_Absolute),
-    HID_ReportCount(5),
-    HID_ReportSize(1),
-    HID_UsagePage(HID_USAGE_PAGE_LED),
-    HID_UsageMin(HID_USAGE_LED_NUM_LOCK),
-    HID_UsageMax(HID_USAGE_LED_KANA),
-    HID_Output(HID_Data | HID_Variable | HID_Absolute),
-    HID_ReportCount(1),
-    HID_ReportSize(3),
-    HID_Output(HID_Data | HID_Variable | HID_Absolute),
-    HID_ReportCount(6),
-    HID_ReportSize(8),
-    HID_LogicalMin(0),
-    HID_LogicalMax(101),
-    HID_UsagePage(HID_USAGE_PAGE_KEYBOARD),
-    HID_UsageMin(0),
-    HID_UsageMax(101),
-    HID_Input(HID_Data | HID_Variable | HID_Absolute),
-  HID_EndCollection,
-};*/
- 
+
 // Called during USBD_Initialize to initialize the USB HID class instance.
 void USBD_HID0_Initialize (void) {
   // Add code for initialization
