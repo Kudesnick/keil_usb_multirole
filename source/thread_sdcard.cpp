8472db06744e4d8fabd82d067be778df00b449de
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2019-12-01 17:48:20 +0300

    [fix] corrected memory drive scanner

--- source/thread_sdcard.cpp
+++ source/thread_sdcard.cpp
@@ -45,56 +45,41 @@ using namespace std;
  *                               PRIVATE FUNCTION PROTOTYPES
  **************************************************************************************************/
 
-void _print_result(fsStatus _status, uint8_t _drive, char * _action)
+fsStatus _print_result(const fsStatus _status, const char *_drive, const char *_action)
 {
-    printf("<sd> ");
+    printf("<sd> drive \"%s\" is %s ", _drive, _action);
     
-    if (_status == fsOK)
+    if (_status != fsOK)
     {
-        printf("drive \"%s:\" is %s.\r\n", fs_DevPool[_drive].id, _action);
+        printf("error: ");
     }
-    else
-    {
-        printf("drive \"%s:\" is not %s. Error: ", fs_DevPool[_drive].id, _action);
-        
-        switch(_status)
-        {
-            case fsOK                : break;
-            case fsError             : printf("fsError - Unspecified error"); break;
-            case fsUnsupported       : printf("fsUnsupported - Operation not supported"); break;
-            case fsAccessDenied      : printf("fsAccessDenied - Resource access denied"); break;
-            case fsInvalidParameter  : printf("fsInvalidParameter - Invalid parameter specified"); break;
-            case fsInvalidDrive      : printf("fsInvalidDrive - Nonexistent drive"); break;
-            case fsInvalidPath       : printf("fsInvalidPath - Invalid path specified"); break;
-            case fsUninitializedDrive: printf("fsUninitializedDrive - Drive is uninitialized"); break;
-            case fsDriverError       : printf("fsDriverError - Read/write error"); break;
-            case fsMediaError        : printf("fsMediaError - Media error"); break;
-            case fsNoMedia           : printf("fsNoMedia - No media, or not initialized"); break;
-            case fsNoFileSystem      : printf("fsNoFileSystem - File system is not formatted"); break;
-            case fsNoFreeSpace       : printf("fsNoFreeSpace - No free space available"); break;
-            case fsFileNotFound      : printf("fsFileNotFound - Requested file not found"); break;
-            case fsDirNotEmpty       : printf("fsDirNotEmpty - The directory is not empty"); break;
-            case fsTooManyOpenFiles  : printf("fsTooManyOpenFiles - Too many open files"); break;
-            case fsAlreadyExists     : printf("File or directory already exists"); break;
-            case fsNotDirectory      : printf("Path is not a directory"); break;
-        }
-        
-        printf("\r\n");
-    }
-}
 
-uint8_t _set_drive_name(uint8_t _drive_num, char * _name)
-{
-    if (_drive_num >= fs_ndrv)
+    switch(_status)
     {
-        _drive_num = 0;
+        case fsOK                : printf("fsOK"); break;
+        case fsError             : printf("fsError - Unspecified error"); break;
+        case fsUnsupported       : printf("fsUnsupported - Operation not supported"); break;
+        case fsAccessDenied      : printf("fsAccessDenied - Resource access denied"); break;
+        case fsInvalidParameter  : printf("fsInvalidParameter - Invalid parameter specified"); break;
+        case fsInvalidDrive      : printf("fsInvalidDrive - Nonexistent drive"); break;
+        case fsInvalidPath       : printf("fsInvalidPath - Invalid path specified"); break;
+        case fsUninitializedDrive: printf("fsUninitializedDrive - Drive is uninitialized"); break;
+        case fsDriverError       : printf("fsDriverError - Read/write error"); break;
+        case fsMediaError        : printf("fsMediaError - Media error"); break;
+        case fsNoMedia           : printf("fsNoMedia - No media, or not initialized"); break;
+        case fsNoFileSystem      : printf("fsNoFileSystem - File system is not formatted"); break;
+        case fsNoFreeSpace       : printf("fsNoFreeSpace - No free space available"); break;
+        case fsFileNotFound      : printf("fsFileNotFound - Requested file not found"); break;
+        case fsDirNotEmpty       : printf("fsDirNotEmpty - The directory is not empty"); break;
+        case fsTooManyOpenFiles  : printf("fsTooManyOpenFiles - Too many open files"); break;
+        case fsAlreadyExists     : printf("fsAlreadyExists - File or directory already exists"); break;
+        case fsNotDirectory      : printf("fsNotDirectory - Path is not a directory"); break;
     }
+
+    printf(".\r\n");
     
-    memcpy(_name, fs_DevPool[_drive_num].id, sizeof(fs_DevPool[0].id) - 1);
-    memcpy(_name + sizeof(fs_DevPool[0].id) - 1, ":", sizeof(":"));
-    
-    return _drive_num;
-};
+    return _status;
+}
 
 /***************************************************************************************************
  *                                       PRIVATE DATA
@@ -121,22 +106,40 @@ class : public cpp_os_thread<SDCARD_STK_SZ>
 private:
     void thread_func(void)
     {
-        // Работаем с картой памяти        
+        // Собираем информацию обо всех дисках     
         for (auto i = 0; i < fs_ndrv; i++)
         {
-            char label[32];
-            char drive[sizeof(fs_DevPool[0].id) + 1] = "MX:";
-            _set_drive_name(i, drive);
-            printf("<sd> %s init...\r\n", drive);
-            fchdrive(drive);
+            char drive[4];
+            sprintf(drive, "%2s:", fs_DevPool[i].id);
+            
+            printf("<sd> %s init..\r\n", drive);
+            
+            if (_print_result(fchdrive(drive), drive, (const char *)"fchdrive") != fsOK)
+            {
+                continue;
+            }
 
-            _print_result(finit (""), i, (char *)"intialised");
-            _print_result(fmount(""), i, (char *)"mounted"); 
-            fvol("", label, NULL);
-            printf("<sd> label: \"%s\"\r\n", label);
+            if (_print_result(finit(drive), drive, (const char *)"finit") != fsOK)
+            {
+                continue;
+            }
+            
+            if (_print_result(fmount(drive), drive, (const char *)"fmount") != fsOK)
+            {
+                continue;
+            }
+            
+            char label[12];
+            uint32_t serial;
+            if (_print_result(fvol(drive, label, &serial), drive, (const char *)"fmount") != fsOK)
+            {
+                continue;
+            }
+            
+            printf("<sd> drive \"%s\" label: \"%s\", serial number: %x.\r\n", drive, label, serial);
         }
 
-        printf("<sd> fs_fat_fh_cnt %d\r\n", fs_fat_fh_cnt);
+        printf("<sd> fs_fat_fh_cnt = %d\r\n", fs_fat_fh_cnt);
 
         for(;;){};
         
